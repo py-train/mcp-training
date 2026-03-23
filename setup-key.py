c7d51aba58996ee26190eb40a7057fc22dd7fa1b
Ashish Gulati <ashishgulati@Ashishs-MacBook-Air-3.local>
2026-03-23 17:01:19 +0530

    Basic setup

---
+++ setup-key.py
@@ -0,0 +1,21 @@
+test_data = [
+  'hello',
+  'world',
+  'rpqkhuR',
+  'rRmQ5BnJAKXZ6KEX2',
+  '2EiY3j8vUIJguZHXu18KZ9GLTPdlsaOXqnjo6Sevlefa1zd1T3BlbkFJYgInPdH3Cb6EfdKsTqNzRrrhTHfRcGBDbHAamhZp',
+  '_2wC8dxDxNk8Gm6pWYG8TU07jF6sCvP8A'
+ ]
+
+test_data = [
+    'hello',
+    'world',
+    'api03',
+    '2CCwJwy8tANhuCCdQNiuurIa',
+    '6lyPTprejMrMN0OWppp4tnUQ0v2Ec0eUneYXNCDqfklFjbUnEvO',
+    'lRdwjbSYw',
+    '9uTHowAA',
+]
+
+print('sk-ant-' + '-'.join(test_data[2:]))
+
